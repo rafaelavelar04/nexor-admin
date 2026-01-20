a38695e70f905bcef33d965caa85907b1cd72ce3
rafaelavelar <rafaelavelar2005@hotmail.com>
2026-01-20 13:45:25 -0300

    fix(auth): corrige perfil inativo, loop de sessão e RLS recursivo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sjp0akuf4lo"
+    "revision": "0.a45efeu36gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
