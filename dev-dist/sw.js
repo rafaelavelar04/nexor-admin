10b200cdf635808f4a809cb4d1480973db5730f6
rafaelavelar <rafaelavelar2005@hotmail.com>
2026-01-19 22:37:09 -0300

    ajustes RLS e estabilidade do admin

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tplk4crmqqg"
+    "revision": "0.sspd937i2go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
